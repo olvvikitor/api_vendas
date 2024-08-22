8388d98f67c1dee5a0aa70921c6f40d5bbdbd7fb
olvvikitor <victoroliveira.3002@gmail.com>
2024-08-22 01:46:12 -0300

    Recursos de usuario implementados-2

--- src/modules/users/services/UpdateProfileUserService.ts
+++ src/modules/users/services/UpdateProfileUserService.ts
@@ -6,7 +6,7 @@ import User from "../typeorm/entities/User";
 interface IRequest {
   user_id: string;
   name: string;
-  email: string;
+  email?: string;
   password?: string;
   old_password?: string;
 }
@@ -23,30 +23,30 @@ class UpdateProfileUserService {
     if (!user) {
       throw new AppError("User not found", 404);
     }
+    if (email) {
+      //retorna true caso seja o mesmo id, e false csao n seja encontrado
+      const emailExists = await userRepository
+        .findByEmail(email)
+        .then((resultado) => user?.id == resultado?.id);
 
-    //retorna true caso seja o mesmo id, e false csao n seja encontrado
-    const emailExists = await userRepository
-      .findByEmail(email)
-      .then((resultado) => user?.id == resultado?.id);
-    if (!emailExists) {
-      throw new AppError("Email already in use", 409);
-    }
-    if(password && !old_password){
-      throw new AppError("Old password is required", 400);
-    }
-    if(password && old_password){
-      const passwordIsMatch = await compare(old_password, user.password);
-      if (!passwordIsMatch) {
-        throw new AppError("Incorrect old password", 401);
+      if (!emailExists) {
+        throw new AppError("Email already in use", 409);
       }
-       user.password = await hash(password, 8);
+
+      if (password && !old_password) {
+        throw new AppError("Old password is required", 400);
+      }
+      if (password && old_password) {
+        const passwordIsMatch = await compare(old_password, user.password);
+        if (!passwordIsMatch) {
+          throw new AppError("Incorrect old password", 401);
+        }
+        user.password = await hash(password, 8);
+      }
+
+      (user.name = name), (user.email = email), await userRepository.save(user);
+
     }
-
-      user.name = name,
-      user.email = email,
-      
-      await userRepository.save(user);
-
     return user;
   }
 }
